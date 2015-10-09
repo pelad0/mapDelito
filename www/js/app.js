88326958c87d6af61c1d959bffaeeb8677cfdb3b
pelad0 <latintainc@gmail.com>
2015-10-09 15:47:44 -0300

    formateo de codigo

--- www/js/app.js
+++ www/js/app.js
@@ -7,71 +7,71 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'leaflet-directive', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app cann be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleLightContent();
+      }
+    });
   })
 
-  // Each tab has its own nav history stack:
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  .state('tab.mapa', {
-    url: '/mapa',
-    views: {
-      'tab-mapa': {
-        templateUrl: 'templates/tab-mapa.html',
-        controller: 'MapaCtrl'
-      }
-    }
-  })
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app cann be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  .state('tab.login', {
-      url: '/login',
-      views: {
-        'tab-login': {
-          templateUrl: 'templates/tab-login.html',
-          controller: 'LoginCtrl'
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+    // Each tab has its own nav history stack:
+
+      .state('tab.mapa', {
+        url: '/mapa',
+        views: {
+          'tab-mapa': {
+            templateUrl: 'templates/tab-mapa.html',
+            controller: 'MapaCtrl'
+          }
         }
-      }
-    })
+      })
 
-    
-   .state('tab.perfil', {
-      url:'/perfil',
-      views:{
-        'tab-perfil':{
-          templateUrl:'templates/tab-perfil.html',
-          controller:'PerfilCtrl'
+      .state('tab.login', {
+        url: '/login',
+        views: {
+          'tab-login': {
+            templateUrl: 'templates/tab-login.html',
+            controller: 'LoginCtrl'
+          }
         }
-        
-      }
-      
-    })
+      })
+
+
+      .state('tab.perfil', {
+        url: '/perfil',
+        views: {
+          'tab-perfil': {
+            templateUrl: 'templates/tab-perfil.html',
+            controller: 'PerfilCtrl'
+          }
+
+        }
+
+      })
 
     /*.state('tab.registro', {
       url: '/registro',
@@ -84,7 +84,7 @@ angular.module('starter', ['ionic', 'leaflet-directive', 'starter.controllers', 
     });*/
 
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/mapa')
-  
-});
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/mapa')
+
+  });
